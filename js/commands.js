46476511648717a5081e1ce85571ec170ee449ae
Mohi Mishra <62807322+mishra016@users.noreply.github.com>
2024-02-22 01:26:34 -0500

    Update commands.js

--- js/commands.js
+++ js/commands.js
@@ -2,7 +2,8 @@
 var password = "mmcodes";
 var linkedin = "https://www.linkedin.com/in/mohi-mishra/";
 var github = "https://github.com/mishra016";
-var reume = "Resume/MishraMohi_CV.pdf";
+var repo = "https://github.com/mishra016/terminal-portfolio";
+var resume = "https://github.com/mishra016/terminal-portfolio/blob/main/Resume/MishraMohi_CV.pdf";
 
 about = [
   "<br>",
@@ -38,6 +39,10 @@ social = [
   "<br>"
 ];
 
+resume = [
+  resume
+];
+
 secret = [
   "<br>",
   '<span class="command">sudo</span>           Only use if you\'re admin',
@@ -58,12 +63,13 @@ help = [
   '<span class="command">whoami</span>         About current user?',
   '<span class="command">education</span>      My education background',
   '<span class="command">social</span>         Display social networks',
+  '<span class="command">resume</span>         View my resume',
   '<span class="command">secret</span>         Find the password',
   '<span class="command">projects</span>       View projects that I have coded',
   '<span class="command">history</span>        View command history',
   '<span class="command">help</span>           Check available commands',
   '<span class="command">clear</span>          Clear terminal',
-  '<span class="command">welcome</span>         Display the header',
+  '<span class="command">welcome</span>       Display the header',
   "<br>",
 ];
 
@@ -81,6 +87,12 @@ welcome = [
   "      ,;;;;;;;;;;;;;;;;;;;;;;;;,.   ",
   "~~,;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
   '<span class="color2">Welcome to my terminal portfolio.</span>',
+  "<br>",
+  '<span class="color2">-----</span>',
+  "<span class=\"color2\">This project's source code can be found in this project's</span>",
+  '<a href="' + repo + '" target="_blank">Github Repo' + "</a><span class=\"color2\">.</span>",
+  '<span class="color2">-----</span>',
+  "<br>",
   "<span class=\"color2\">Type</span> <span class=\"command\">'help'</span><span class=\"color2\"> to see the list of avilable commands.</span>",
 ];
 
